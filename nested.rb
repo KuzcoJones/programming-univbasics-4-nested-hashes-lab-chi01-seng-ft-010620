6fcbf23908f5fdc9467df559b2563f3ceb1c51bb
Keenan Jones <keenanjones210@gmail.com>
2019-12-25 05:58:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }return programmer_hash[dennis_ritchie][languages]
+     }
+     return programmer_hash[dennis_ritchie][languages]
      
 end
 
